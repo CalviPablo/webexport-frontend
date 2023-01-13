6c18ca58544fe9eecd96ce6999f89ab24a92d027
Pablo <96096465+CalviPablo@users.noreply.github.com>
2023-01-13 19:21:26 -0300

    Gitignore

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // PROD
-export const URL_BACKEND = "https://pcalvi.online/public/api";
+// export const URL_BACKEND = "https://pcalvi.online/public/api";
 
 // DESARROLLO
-// export const URL_BACKEND = "http://127.0.0.1:8000/api";
+export const URL_BACKEND = "http://127.0.0.1:8000/api";

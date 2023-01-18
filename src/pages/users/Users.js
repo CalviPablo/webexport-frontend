842bbbe479c3273fb59c839df57db67d3e449a75
Pablo <96096465+CalviPablo@users.noreply.github.com>
2023-01-17 22:05:06 -0300

    Loading spinner

--- src/pages/users/Users.js
+++ src/pages/users/Users.js
@@ -2,17 +2,20 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 import { URL_BACKEND } from "../../config";
+import "../../css/spinner.css";
 
 export default function Users() {
   const [users, setUsers] = useState([]);
   const [totalPages, setTotalPages] = useState(1);
   const [currentPage, setCurrentPage] = useState([1]);
+  const [isLoaded, setIsLoaded] = useState([false]);
 
   // Devuelve los usuarios desde la api.
   useEffect(() => {
     axios.get(`${URL_BACKEND}/users?page=${currentPage}`).then((res) => {
       setUsers(res.data.data);
       setTotalPages(res.data.last_page);
+      setIsLoaded(true);
       // console.log(totalPages);
       // console.log(users);
       // console.log(res.data);
@@ -50,75 +53,80 @@ export default function Users() {
 
   return (
     <div className="container">
-      <a href="/users/create" className="btn btn-secondary mt-3 mb-3">
-        Agregar User
-      </a>
-      <div className="table-responsive">
-        <table className="table table-striped table-hover table-bordered">
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Correo</th>
-              <th>Rol</th>
-              <th>Editar</th>
-              <th>Borrar</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => {
-              return (
-                <tr key={user.id}>
-                  <td>{user.user}</td>
-                  <td>{user.email}</td>
-                  <td>{user.role.role_name}</td>
-                  <td>
-                    <a
-                      href={"users/edit/" + user.id}
-                      className="btn btn-secondary btn-sm"
-                    >
-                      Editar
-                    </a>
-                  </td>
-                  <td>
-                    <button
-                      className="btn btn-secondary btn-sm"
-                      onClick={() => deleteUser(user.id)}
-                    >
-                      Borrar
-                    </button>
-                  </td>
+      {!isLoaded && <div class="lds-dual-ring"></div>}
+      {isLoaded && (
+        <>
+          <a href="/users/create" className="btn btn-secondary mt-3 mb-3">
+            Agregar User
+          </a>
+          <div className="table-responsive">
+            <table className="table table-striped table-hover table-bordered">
+              <thead>
+                <tr>
+                  <th>Nombre</th>
+                  <th>Correo</th>
+                  <th>Rol</th>
+                  <th>Editar</th>
+                  <th>Borrar</th>
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        <nav aria-label="Page navigation example">
-          <ul className="pagination">
-            <li className="page-item">
-              <button className="page-link" onClick={() => prevPage()}>
-                Previous
-              </button>
-            </li>
-            {Array.from({ length: totalPages }, (_, i) => {
-              return (
-                <li key={i} className="page-item">
-                  <button
-                    className="page-link"
-                    onClick={() => changePage(i + 1)}
-                  >
-                    {i + 1}
+              </thead>
+              <tbody>
+                {users.map((user) => {
+                  return (
+                    <tr key={user.id}>
+                      <td>{user.user}</td>
+                      <td>{user.email}</td>
+                      <td>{user.role.role_name}</td>
+                      <td>
+                        <a
+                          href={"users/edit/" + user.id}
+                          className="btn btn-secondary btn-sm"
+                        >
+                          Editar
+                        </a>
+                      </td>
+                      <td>
+                        <button
+                          className="btn btn-secondary btn-sm"
+                          onClick={() => deleteUser(user.id)}
+                        >
+                          Borrar
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+            <nav aria-label="Page navigation example">
+              <ul className="pagination">
+                <li className="page-item">
+                  <button className="page-link" onClick={() => prevPage()}>
+                    Previous
                   </button>
                 </li>
-              );
-            })}
-            <li className="page-item">
-              <button className="page-link" onClick={() => nextPage()}>
-                Next
-              </button>
-            </li>
-          </ul>
-        </nav>
-      </div>
+                {Array.from({ length: totalPages }, (_, i) => {
+                  return (
+                    <li key={i} className="page-item">
+                      <button
+                        className="page-link"
+                        onClick={() => changePage(i + 1)}
+                      >
+                        {i + 1}
+                      </button>
+                    </li>
+                  );
+                })}
+                <li className="page-item">
+                  <button className="page-link" onClick={() => nextPage()}>
+                    Next
+                  </button>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </>
+      )}
     </div>
   );
 }
